afbe2758476931e7023f0cfb7959df9d8c609c3b
Sandy Kee <sandykee95@hotmail.com>
2019-12-01 23:11:40 +0800

    #7 Uncommented test cases and added test outputs
    Test cases was commented - uncommented it. A picture of the test outputs for the login test cases was also added.

--- test_cases_login.py
+++ test_cases_login.py
@@ -24,127 +24,127 @@ def test_login_without_entering_username_or_password():
     assert driver.find_element_by_css_selector("input:invalid")
     driver.quit()
     
-##def test_login_without_entering_username():
-##    driver = webdriver.Chrome()
-##    driver.maximize_window()
-##    driver.get('http://localhost:8000')
-##    time.sleep(1)
-##    button = driver.find_element_by_tag_name('a')
-##    button.click()
-##
-##    time.sleep(1)
-##    password = driver.find_element_by_id('id_password')
-##    password.send_keys("admin123")
-##    button = driver.find_element_by_xpath("//button")
-##    button.click()
-##    assert driver.find_element_by_css_selector("input:invalid")
-##    driver.quit()
-##
-##def test_login_without_entering_password():
-##    driver = webdriver.Chrome()
-##    driver.maximize_window()
-##    driver.get('http://localhost:8000')
-##    time.sleep(1)
-##    button = driver.find_element_by_tag_name('a')
-##    button.click()
-##
-##    time.sleep(1)
-##    username = 'admin'
-##    driver.find_element_by_id('id_username').send_keys(username)
-##    button = driver.find_element_by_xpath("//button")
-##    button.click()
-##    time.sleep(2)
-##    assert driver.find_element_by_css_selector("input:invalid")
-##    driver.quit()
-##
-##def test_login_with_incorrect_username():
-##    driver = webdriver.Chrome()
-##    driver.maximize_window()
-##    driver.get('http://localhost:8000')
-##    time.sleep(1)
-##    button = driver.find_element_by_tag_name('a')
-##    button.click()
-##
-##    time.sleep(1)
-##    username = 'admin?'
-##    driver.find_element_by_id('id_username').send_keys(username)
-##    password = driver.find_element_by_id('id_password')
-##    password.send_keys("admin123")
-##    button = driver.find_element_by_xpath("//button")
-##    button.click()
-##    time.sleep(2)
-##    assert "The username or password you have entered is incorrect, please try again." in driver.page_source
-##    driver.quit()
-##
-##def test_login_with_incorrect_password():
-##    driver = webdriver.Chrome()
-##    driver.maximize_window()
-##    driver.get('http://localhost:8000')
-##    time.sleep(1)
-##    button = driver.find_element_by_tag_name('a')
-##    button.click()
-##
-##    time.sleep(1)
-##    username = 'admin'
-##    driver.find_element_by_id('id_username').send_keys(username)
-##    password = driver.find_element_by_id('id_password')
-##    password.send_keys("admin123?")
-##    button = driver.find_element_by_xpath("//button")
-##    button.click()
-##    time.sleep(2)
-##    assert "The username or password you have entered is incorrect, please try again." in driver.page_source
-##    driver.quit()
-##
-##def test_successful_login():
-##    driver = webdriver.Chrome()
-##    driver.maximize_window()
-##    driver.get('http://localhost:8000')
-##    time.sleep(1)
-##    button = driver.find_element_by_tag_name('a')
-##    button.click()
-##
-##    time.sleep(2)
-##    username = 'admin'
-##    driver.find_element_by_id('id_username').send_keys(username)
-##    password = driver.find_element_by_id('id_password')
-##    password.send_keys("admin123")
-##    button = driver.find_element_by_xpath("//button")
-##    button.click()
-##    time.sleep(2)
-##    assert "You are logged in as " + username + "." in driver.page_source
-##    driver.quit()
-##    
-##def test_navigation_to_login_page():
-##    driver = webdriver.Chrome()
-##    driver.maximize_window()
-##    driver.get('http://localhost:8000')
-##    assert "You are not logged in." in driver.page_source
-##
-##    time.sleep(2)
-##    button = driver.find_element_by_tag_name('a')
-##    button.click()
-##    time.sleep(2)
-##    assert "Login" in driver.page_source
-##    driver.quit()
-##
-##def test_logout():
-##    driver = webdriver.Chrome()
-##    driver.maximize_window()
-##    driver.get('http://localhost:8000')
-##    time.sleep(1)
-##    button = driver.find_element_by_tag_name('a')
-##    button.click()
-##
-##    time.sleep(2)
-##    username = 'admin'
-##    driver.find_element_by_id('id_username').send_keys(username)
-##    password = driver.find_element_by_id('id_password')
-##    password.send_keys("admin123")
-##    button = driver.find_element_by_xpath("//button")
-##    button.click()
-##    
-##    logout = driver.find_element_by_tag_name('a')
-##    logout.click()
-##    time.sleep(2)
-##    assert "You are not logged in." in driver.page_source
-##    driver.quit()
+def test_login_without_entering_username():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get('http://localhost:8000')
+    time.sleep(1)
+    button = driver.find_element_by_tag_name('a')
+    button.click()
+
+    time.sleep(1)
+    password = driver.find_element_by_id('id_password')
+    password.send_keys("admin123")
+    button = driver.find_element_by_xpath("//button")
+    button.click()
+    assert driver.find_element_by_css_selector("input:invalid")
+    driver.quit()
+
+def test_login_without_entering_password():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get('http://localhost:8000')
+    time.sleep(1)
+    button = driver.find_element_by_tag_name('a')
+    button.click()
+
+    time.sleep(1)
+    username = 'admin'
+    driver.find_element_by_id('id_username').send_keys(username)
+    button = driver.find_element_by_xpath("//button")
+    button.click()
+    time.sleep(2)
+    assert driver.find_element_by_css_selector("input:invalid")
+    driver.quit()
+
+def test_login_with_incorrect_username():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get('http://localhost:8000')
+    time.sleep(1)
+    button = driver.find_element_by_tag_name('a')
+    button.click()
+
+    time.sleep(1)
+    username = 'admin?'
+    driver.find_element_by_id('id_username').send_keys(username)
+    password = driver.find_element_by_id('id_password')
+    password.send_keys("admin123")
+    button = driver.find_element_by_xpath("//button")
+    button.click()
+    time.sleep(2)
+    assert "The username or password you have entered is incorrect, please try again." in driver.page_source
+    driver.quit()
+
+def test_login_with_incorrect_password():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get('http://localhost:8000')
+    time.sleep(1)
+    button = driver.find_element_by_tag_name('a')
+    button.click()
+
+    time.sleep(1)
+    username = 'admin'
+    driver.find_element_by_id('id_username').send_keys(username)
+    password = driver.find_element_by_id('id_password')
+    password.send_keys("admin123?")
+    button = driver.find_element_by_xpath("//button")
+    button.click()
+    time.sleep(2)
+    assert "The username or password you have entered is incorrect, please try again." in driver.page_source
+    driver.quit()
+
+def test_successful_login():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get('http://localhost:8000')
+    time.sleep(1)
+    button = driver.find_element_by_tag_name('a')
+    button.click()
+
+    time.sleep(2)
+    username = 'admin'
+    driver.find_element_by_id('id_username').send_keys(username)
+    password = driver.find_element_by_id('id_password')
+    password.send_keys("admin123")
+    button = driver.find_element_by_xpath("//button")
+    button.click()
+    time.sleep(2)
+    assert "You are logged in as " + username + "." in driver.page_source
+    driver.quit()
+    
+def test_navigation_to_login_page():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get('http://localhost:8000')
+    assert "You are not logged in." in driver.page_source
+
+    time.sleep(2)
+    button = driver.find_element_by_tag_name('a')
+    button.click()
+    time.sleep(2)
+    assert "Login" in driver.page_source
+    driver.quit()
+
+def test_logout():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get('http://localhost:8000')
+    time.sleep(1)
+    button = driver.find_element_by_tag_name('a')
+    button.click()
+
+    time.sleep(2)
+    username = 'admin'
+    driver.find_element_by_id('id_username').send_keys(username)
+    password = driver.find_element_by_id('id_password')
+    password.send_keys("admin123")
+    button = driver.find_element_by_xpath("//button")
+    button.click()
+    
+    logout = driver.find_element_by_tag_name('a')
+    logout.click()
+    time.sleep(2)
+    assert "You are not logged in." in driver.page_source
+    driver.quit()
