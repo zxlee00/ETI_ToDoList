73206e5248cb8a68ca01ef4801c1057d5e6cb6e0
Lee Zhi Xuan <zxnlee00@gmail.com>
2019-12-21 22:43:56 +0800

    #20 Added team contributions page and navigation to the team contributions page

--- ETIToDoList/todolist/urls.py
+++ ETIToDoList/todolist/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('teamcontributions/', TemplateView.as_view(template_name='teamcontributions.html'), name='teamcontributions'),
 
     #todo Pages
     path('todo/', todo_view),
